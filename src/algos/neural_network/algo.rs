67c6291ee4a1b01f5c8a9996ea4f49dfcd38f774
ktran <kevin_tran@live.fr>
2020-04-18 13:23:45 +0200

    NN learn method

--- src/algos/neural_network/algo.rs
+++ src/algos/neural_network/algo.rs
@@ -6,13 +6,15 @@ use tensorflow::train::Optimizer;
 pub struct NeuralNetwork {
     session: tf::Session,
     input: tf::Operation,
-    probabilities: tf::Output,
+    probabilities: tf::Operation,
     chosen_action: tf::Operation,
     action_holder: tf::Operation,
     reward_holder: tf::Operation,
     minimize: tf::Operation,
     loss: tf::Operation,
 }
+
+type InputType = tf::Tensor<f64>;
 
 impl NeuralNetwork {
     pub fn new(dimensions: &[u64]) -> Result<NeuralNetwork, tf::Status> {
@@ -36,18 +38,49 @@ impl NeuralNetwork {
         })
     }
 
-    pub fn play(&self, state: &[usize]) -> Result<i64, tf::Status> {
+    pub fn build_input(&self, state: &[usize]) -> InputType {
         let mut input_tensor = tf::Tensor::new(&[1, 19]);
         for index in state {
             input_tensor[*index] = 1.0;
         }
+        input_tensor
+    }
 
+    pub fn play(&self, input_tensor: &InputType) -> Result<i64, tf::Status> {
         let mut run_args = tf::SessionRunArgs::new();
         let action_fetch = run_args.request_fetch(&self.chosen_action, 0);
         run_args.add_feed(&self.input, 0, &input_tensor);
         self.session.run(&mut run_args)?;
         let action = run_args.fetch::<i64>(action_fetch)?[0];
         Ok(action)
+    }
+
+    pub fn learn(&self, input_tensor: &InputType, action: i64, reward: f64) -> Result<f64, tf::Status> {
+        let action_value = tf::Tensor::new(&[1][..]).with_values(&[action as i32])?;
+        let reward_value = tf::Tensor::new(&[1][..]).with_values(&[reward])?;
+        let mut run_args = tf::SessionRunArgs::new();
+        run_args.add_target(&self.minimize);
+        run_args.add_feed(&self.input, 0, &input_tensor);
+        run_args.add_feed(&self.action_holder, 0, &action_value);
+        run_args.add_feed(&self.reward_holder, 0, &reward_value);
+        let loss_fetch = run_args.request_fetch(&self.loss, 0);
+        self.session.run(&mut run_args)?;
+        let loss: f64 = run_args.fetch(loss_fetch)?[0];
+        Ok(loss)
+    }
+
+    pub fn get_probabilities(&self, input_tensor: &InputType) -> Result<Vec<f64>, tf::Status> {
+        let mut result = Vec::new();
+        let mut run_args = tf::SessionRunArgs::new();
+        let probabilities_fetch = run_args.request_fetch(&self.probabilities, 0);
+        run_args.add_feed(&self.input, 0, &input_tensor);
+        self.session.run(&mut run_args)?;
+        let probabilities_value = run_args.fetch::<f64>(probabilities_fetch)?;
+        let size = probabilities_value.dims()[1] as usize;
+        for i in 0..size {
+            result.push(probabilities_value[i]);
+        }
+        Ok(result)
     }
 }
 
@@ -69,7 +102,7 @@ fn initialize(
 
 fn setup_training(
     scope: &tf::Scope,
-    output_layer: &tf::Output,
+    output_layer: &tf::Operation,
     variables: &Vec<tf::Variable>,
 ) -> Result<(
     Vec<tf::Variable>,
@@ -89,7 +122,7 @@ fn setup_training(
         .build(&mut scope.with_op_name("action_holder"))?;
     let slice_size = tf_ops::constant(&[1][..], &mut scope)?;
     let responsible_weight = tf_ops::slice(
-        tf_ops::squeeze(output_layer.clone(), &mut scope)?.into(),
+        tf_ops::squeeze(output_layer.clone().into(), &mut scope)?.into(),
         action_holder.clone().into(),
         slice_size.into(),
         &mut scope,
@@ -114,7 +147,7 @@ fn setup_training(
 fn build_model(
     scope: &tf::Scope,
     dimensions: &[u64],
-) -> Result<(Vec<tf::Variable>, tf::Operation, tf::Output, tf::Operation), tf::Status> {
+) -> Result<(Vec<tf::Variable>, tf::Operation, tf::Operation, tf::Operation), tf::Status> {
     let mut scope = scope.new_sub_scope("model");
     let input = tf_ops::Placeholder::new()
         .dtype(tf::DataType::Double)
@@ -128,7 +161,7 @@ fn build_model(
             layer.clone(),
             dimensions[index],
             dimensions[index + 1],
-            &|x, scope| Ok(tf_ops::tanh(x, scope)?.into()),
+            &|x, scope| Ok(tf_ops::tanh(x, scope)?),
         )?;
         variables.extend(vars);
     }
@@ -138,24 +171,24 @@ fn build_model(
         layer.clone(),
         dimensions[last_index],
         dimensions[last_index + 1],
-        &|x, scope| Ok(tf_ops::softmax(x, scope)?.into()),
+        &|x, scope| Ok(tf_ops::softmax(x, scope)?),
     )?;
     variables.extend(vars);
     let chosen_action = tf_ops::arg_max(
-        layer.clone(),
+        layer.clone().into(),
         tf_ops::constant(1, &mut scope)?.into(),
         &mut scope.with_op_name("chosen_action"),
     )?;
     Ok((variables, input, layer, chosen_action))
 }
 
-fn build_layer<IntoOutput: Into<tf::Output>>(
+fn build_layer(
     scope: &tf::Scope,
-    input: IntoOutput,
+    input: tf::Operation,
     input_size: u64,
     output_size: u64,
-    activation: &dyn Fn(tf::Output, &mut tf::Scope) -> Result<tf::Output, tf::Status>,
-) -> Result<(Vec<tf::Variable>, tf::Output), tf::Status> {
+    activation: &dyn Fn(tf::Output, &mut tf::Scope) -> Result<tf::Operation, tf::Status>,
+) -> Result<(Vec<tf::Variable>, tf::Operation), tf::Status> {
     let mut scope = scope.new_sub_scope("layer");
     let weight_shape = tf_ops::constant(&[input_size as i64, output_size as i64][..], &mut scope)?;
     let weights = tf::Variable::builder()
@@ -191,8 +224,10 @@ mod tests {
     #[test]
     fn test_build_graph() -> Result<(), Box<dyn error::Error>> {
         let nn = NeuralNetwork::new(&[19, 9])?;
-        let action = nn.play(&[0])?;
-        assert_eq!(action, -1);
+        let input = nn.build_input(&[0]);
+        let action = nn.play(&input)?;
+        nn.learn(&input, action, 1.0)?;
+        nn.get_probabilities(&input);
         Ok(())
     }
 }
