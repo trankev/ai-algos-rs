466c5c3a146be7c3fb56b3c26bad78a519a0104f
ktran <kevin_tran@live.fr>
2020-04-18 12:00:36 +0200

    Basic neural network

--- src/algos/mod.rs
+++ src/algos/mod.rs
@@ -1,2 +1,3 @@
 pub mod minimax;
+pub mod neural_network;
 pub mod proba_learning;
---
+++ src/algos/neural_network/algo.rs
@@ -0,0 +1,180 @@
+use tensorflow as tf;
+use tensorflow::ops as tf_ops;
+use tensorflow::train as tf_train;
+use tensorflow::train::Optimizer;
+
+pub struct NeuralNetwork {
+    session: tf::Session,
+    probabilities: tf::Output,
+    chosen_action: tf::Operation,
+    action_holder: tf::Operation,
+    reward_holder: tf::Operation,
+    minimize: tf::Operation,
+    loss: tf::Operation,
+}
+
+impl NeuralNetwork {
+    pub fn new(dimensions: &[u64]) -> Result<NeuralNetwork, tf::Status> {
+        let mut scope = tf::Scope::new_root_scope();
+        let (variables, output_layer, chosen_action) = build_model(&scope, dimensions)?;
+        let (minimizer_vars, action_holder, reward_holder, loss, minimize) =
+            setup_training(&scope, &output_layer, &variables)?;
+        let options = tf::SessionOptions::new();
+        let graph = scope.graph_mut();
+        let session = tf::Session::new(&options, &graph)?;
+        initialize(&session, &variables, &minimizer_vars)?;
+        Ok(NeuralNetwork {
+            session,
+            probabilities: output_layer,
+            chosen_action,
+            action_holder,
+            reward_holder,
+            minimize,
+            loss,
+        })
+    }
+}
+
+fn initialize(
+    session: &tf::Session,
+    model_variables: &Vec<tf::Variable>,
+    minimizer_variables: &Vec<tf::Variable>,
+) -> Result<(), tf::Status> {
+    let mut run_args = tf::SessionRunArgs::new();
+    for var in model_variables {
+        run_args.add_target(var.initializer());
+    }
+    for var in minimizer_variables {
+        run_args.add_target(var.initializer());
+    }
+    session.run(&mut run_args)?;
+    Ok(())
+}
+
+fn setup_training(
+    scope: &tf::Scope,
+    output_layer: &tf::Output,
+    variables: &Vec<tf::Variable>,
+) -> Result<(
+    Vec<tf::Variable>,
+    tf::Operation,
+    tf::Operation,
+    tf::Operation,
+    tf::Operation,
+), tf::Status> {
+    let mut scope = scope.new_sub_scope("training");
+    let reward_holder = tf_ops::Placeholder::new()
+        .dtype(tf::DataType::Double)
+        .shape(tf::Shape::from(&[1u64][..]))
+        .build(&mut scope.with_op_name("reward_holder"))?;
+    let action_holder = tf_ops::Placeholder::new()
+        .dtype(tf::DataType::Int32)
+        .shape(tf::Shape::from(&[1u64][..]))
+        .build(&mut scope.with_op_name("action_holder"))?;
+    let slice_size = tf_ops::constant(&[1][..], &mut scope)?;
+    let responsible_weight = tf_ops::slice(
+        tf_ops::squeeze(output_layer.clone(), &mut scope)?.into(),
+        action_holder.clone().into(),
+        slice_size.into(),
+        &mut scope,
+    )?;
+    let log = tf_ops::log(responsible_weight.into(), &mut scope)?;
+    let neg = tf_ops::neg(log.into(), &mut scope)?;
+    let loss = tf_ops::mul(
+        neg.into(),
+        reward_holder.clone().into(),
+        &mut scope.with_op_name("loss"),
+    )?;
+    let learning_rate = tf_ops::constant(0.001f64, &mut scope)?;
+    let optimizer = tf_train::GradientDescentOptimizer::new(learning_rate);
+    let (minimizer_vars, minimize) = optimizer.minimize(
+        &mut scope.with_op_name("minimize"),
+        loss.clone().into(),
+        tf_train::MinimizeOptions::default().with_variables(&variables),
+    )?;
+    Ok((minimizer_vars, action_holder, reward_holder, loss, minimize))
+}
+
+fn build_model(
+    scope: &tf::Scope,
+    dimensions: &[u64],
+) -> Result<(Vec<tf::Variable>, tf::Output, tf::Operation), tf::Status> {
+    let mut scope = scope.new_sub_scope("model");
+    let input = tf_ops::Placeholder::new()
+        .dtype(tf::DataType::Double)
+        .shape(tf::Shape::from(&[1u64, dimensions[0]][..]))
+        .build(&mut scope.with_op_name("input"))?;
+    let mut variables = Vec::new();
+    let layer = input;
+    for index in 0..(dimensions.len() - 2) {
+        let (vars, layer) = build_layer(
+            &scope,
+            layer.clone(),
+            dimensions[index],
+            dimensions[index + 1],
+            &|x, scope| Ok(tf_ops::tanh(x, scope)?.into()),
+        )?;
+        variables.extend(vars);
+    }
+    let last_index = dimensions.len() - 2;
+    let (vars, layer) = build_layer(
+        &scope,
+        layer.clone(),
+        dimensions[last_index],
+        dimensions[last_index + 1],
+        &|x, scope| Ok(tf_ops::softmax(x, scope)?.into()),
+    )?;
+    variables.extend(vars);
+    let chosen_action = tf_ops::arg_max(
+        tf_ops::squeeze(layer.clone(), &mut scope)?.into(),
+        tf_ops::constant(1, &mut scope)?.into(),
+        &mut scope.with_op_name("chosen_action"),
+    )?;
+    Ok((variables, layer, chosen_action))
+}
+
+fn build_layer<IntoOutput: Into<tf::Output>>(
+    scope: &tf::Scope,
+    input: IntoOutput,
+    input_size: u64,
+    output_size: u64,
+    activation: &dyn Fn(tf::Output, &mut tf::Scope) -> Result<tf::Output, tf::Status>,
+) -> Result<(Vec<tf::Variable>, tf::Output), tf::Status> {
+    let mut scope = scope.new_sub_scope("layer");
+    let weight_shape = tf_ops::constant(&[input_size as i64, output_size as i64][..], &mut scope)?;
+    let weights = tf::Variable::builder()
+        .initial_value(
+            tf_ops::RandomStandardNormal::new()
+                .dtype(tf::DataType::Double)
+                .build(weight_shape.into(), &mut scope)?
+        )
+        .data_type(tf::DataType::Double)
+        .shape(tf::Shape::from(&[input_size, output_size][..]))
+        .build(&mut scope)?;
+    let biases = tf::Variable::builder()
+        .const_initial_value(tf::Tensor::<f64>::new(&[output_size]))
+        .build(&mut scope)?;
+    Ok((
+        vec![weights.clone(), biases.clone()],
+        activation(
+            tf_ops::add(
+                tf_ops::mat_mul(input.into(), weights.output().clone(), &mut scope)?.into(),
+                biases.output().clone(),
+                &mut scope,
+            )?.into(),
+            &mut scope,
+        )?
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::error;
+
+    #[test]
+    fn test_build_graph() -> Result<(), Box<dyn error::Error>> {
+        NeuralNetwork::new(&[19, 9])?;
+        Ok(())
+    }
+}
